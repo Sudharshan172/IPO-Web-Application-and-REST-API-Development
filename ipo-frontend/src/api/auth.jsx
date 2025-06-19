35d6ed6cc06189a0be095e2e89f425f9ca1c75a9
Sudharshan Reddy <143404435+Sudharshan172@users.noreply.github.com>
2025-06-19 10:37:35 +0530

    Update auth.jsx

--- ipo-frontend/src/api/auth.jsx
+++ ipo-frontend/src/api/auth.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/auth";
+const API_URL = "https://ipo-web-application.onrender.com/auth";
 
 export const registerUser = async (username, password) => {
     return axios.post(`${API_URL}/signupnow/`, { username, password });
